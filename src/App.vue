d00e6f1f39b21981ded9bc70dd3aed857a020b8b
bboysquiz <mrkiraller@gmail.com>
2023-08-25 13:14:15 +0300

    filter in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue';
+
+const search = ref('');
+const status = ref('');
+const characters = ref([
+  {
+    id: 1,
+    name: "Rick Sanchez",
+    "status": "Unknown",
+  },
+  {
+    id: 2,
+    name: "Morty Smith",
+    "status": "Dead",
+  },
+  {
+    id: 3,
+    name: "Summer Smith",
+    "status": "Alive",
+  }
+]);
+const filteredCharacters = computed(() => {
+  let filteredChars = characters.value;
+
+  if (search.value) {
+    filteredChars = filteredChars.filter(character =>
+      character.name.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+  if (status.value) {
+    filteredChars = filteredChars.filter(character =>
+      character.status.toLowerCase() === status.value.toLowerCase()
+    );
+  }
+
+  return filteredChars;
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input v-model="search" @input="filtered" placeholder="Search by name" />
+    <select v-model="status" @change="filtered">
+      <option value="">Any Status</option>
+      <option value="alive">Alive</option>
+      <option value="dead">Dead</option>
+      <option value="unknown">Unknown</option>
+    </select>
+    <ul>
+      <li v-for="character in filteredCharacters" :key="character.id">
+        <router-link :to="{ name: 'character', params: { id: character.id } }">
+          <h2>{{ character.name }}</h2>
+        </router-link>
+        <p>{{ character.species }}</p>
+        <img :src="character.image" :alt="character.name" />
+        <ul>
+          <li v-for="episode in character.episodes" :key="episode">
+            <router-link :to="{ name: 'episode', params: { id: episode } }">
+              Episode {{ episode }}
+            </router-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
